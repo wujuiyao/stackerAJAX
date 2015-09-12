5624c27106380eeb3d6e164544d76d222a3ede44
Richard Wu <contactrichardwu@gmail.com>
2015-09-12 22:37:41 +0800

    add the answerer score

--- app.js
+++ app.js
@@ -6,20 +6,17 @@ $(document).ready( function() {
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
 	});
-
 	$('.inspiration-getter').submit(function(event){
 		$('.results').html('');
 		var answerers = $(this).find("input[name='answerers']").val();
 		console.log(answerers);
 		getAnswers(answerers);
   });
-
 });
-
 // this function takes the question object returned by StackOverflow
 // and creates new result to be appended to DOM
-//ask nicholas, why do they add the showQuestion like that
 
+//ask nicholas, why do they add the showQuestion like that
 var showQuestion = function(question) {
 
 	// clone our result template code
@@ -52,7 +49,6 @@ var showQuestion = function(question) {
 
 // this function takes the results object from StackOverflow
 // and creates info about search results to be appended to DOM
-//ask Nicholas, the query and resultNum, can they be any variable name? what will defined inside that function
 var showSearchResults = function(query, resultNum) {
 	var results = '<br>' + resultNum + ' results for <strong>' + query;
 	return results;
@@ -64,17 +60,14 @@ var showError = function(error){
 	var errorText = '<p>' + error + '</p>';
 	errorElem.append(errorText);
 };
-
 // takes a string of semi-colon separated tags to be searched
 // for on StackOverflow
 var getUnanswered = function(tags) {
-
 	// the parameters we need to pass in our request to StackOverflow's API
 	var request = {tagged: tags,
 								site: 'stackoverflow',
 								order: 'desc',
 								sort: 'creation'};
-
 	var result = $.ajax({
 		url: "http://api.stackexchange.com/2.2/questions/unanswered",
 		data: request,
@@ -98,7 +91,6 @@ var getUnanswered = function(tags) {
 
 var showAnswer = function(answer) {
 
-	// clone our result template code
 	var result = $('.templates .answer').clone();
 
 	// Set the Name of top answerers, include hyperlink
@@ -109,13 +101,13 @@ var showAnswer = function(answer) {
 	// Set the profile image
   var profileImage = result.find('.profile-image a');
 	var imageLink = answer.user.profile_image;
-	console.log(imageLink);
 	profileImage.attr('href', answer.user.link);
 	profileImage.prepend('<img src="'+ imageLink + '" alt="">');
 
 	//Show the score
-	// var viewed = result.find('.viewed');
-	// viewed.text(question.view_count);
+	var score = result.find('.answerer-score');
+	var answererScore = answer.score;
+	score.html(answererScore);
 
 	return result;
 };
@@ -145,5 +137,4 @@ var getAnswers = function(answerers){
 		var errorElem = showError(error);
 		$('.search-results').append(errorElem);
 	});
-
 };
